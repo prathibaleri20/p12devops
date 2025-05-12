af1803ed26f6580c619a2e4ebba2957ed6e6801f
Prathiksha Harish Baleri <122948382+prathibaleri20@users.noreply.github.com>
2025-05-12 11:06:56 +0530

    Add files via upload

---
+++ web/src/main/java/com/example/web/web/App.java
@@ -0,0 +1,14 @@
+package com.example.web.web;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello from Prathiksha and pallavi!" );
+        System.out.println( "we offer a wide range of courses specially for u" );
+    }
+}
